9ff3fca22237d167c2f2d93c3f2ba9c6cb091acd
sieunhann <manhcuong200997@gmail.com>
2022-10-28 18:47:47 +0700

    add ResquetBody for api create order

--- src/main/java/com/example/project/controller/admin/OrderController.java
+++ src/main/java/com/example/project/controller/admin/OrderController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class OrderController {
@@ -25,7 +27,7 @@ public class OrderController {
     }
 
     @PostMapping(value = "/api/v1/admin/order")
-    public ResponseEntity<?> createOrder(OrderCreateRequest request){
+    public ResponseEntity<?> createOrder(@RequestBody OrderCreateRequest request){
         return ResponseEntity.ok(orderService.createOrder(request));
     }
 }
